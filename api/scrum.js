85d9faa33a336258217c9a213022a81505a1748e
John Cudd <jmcudd@gmail.com>
2020-07-29 14:53:08 -0400

    Updated formating to json

--- api/scrum.js
+++ api/scrum.js
@@ -1,11 +1,14 @@
 const DailySort = require("daily-sort");
 module.exports = (req, res) => {
-  res.setHeader("content-type", "text/plain");
+  res.setHeader("content-type", "application/json");
   console.log(req.body);
   const list = DailySort.shuffle(req.body.text.split(" "));
   let output = "";
   list.forEach((name, i) => {
-    output += `${i}. ${name}\n`;
+    output += `${i + 1}. ${name}\n`;
   });
-  res.send(output);
+  res.send({
+    response_type: "in_channel",
+    text: output
+  });
 };
