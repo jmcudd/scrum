a982cda368d17e41aba424e994aefa2e10d0b8f8
John Cudd <jmcudd@gmail.com>
2020-07-29 13:31:03 -0400

    Sort list

--- api/scrum.js
+++ api/scrum.js
@@ -2,6 +2,6 @@ const DailySort = require("daily-sort");
 module.exports = (req, res) => {
   res.setHeader("content-type", "text/plain");
   console.log(req.body)
-  const list = DailySort.shuffle(["John", "Jim", "Bob", "Sally", "Jack"]);
+  const list = DailySort.shuffle(req.body.text.split(" "));
   res.send(`${list}`);
 };
